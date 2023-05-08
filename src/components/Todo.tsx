f8525d28280825d2d84cc2593a17ec253f54b608
anishazahan <anishazahan13@gmail.com>
2023-05-08 17:21:50 +0600

    load single todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -10,12 +10,16 @@ type todoType= {
 
 
 const Todo = ({todo}:todoType) => {
-    console.log(todo);
+    // console.log(todo);
    
   return (
     <div className='todo'>
         <h2>{todo.id}</h2>
         <h2>{todo.title}</h2>
+        <div className="">
+            <button>Edit</button>
+            <button className='delete'>Delete</button>
+        </div>
     </div>
   )
 }
